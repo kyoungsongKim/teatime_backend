c3cecf2db5b7c9d174c062935e2b26a5febaa3d5
HaTaejin <htj1222@gmail.com>
2022-10-05 22:14:00 +0900

    Autowired 제거

--- src/main/java/castis/web/AgentController.java
+++ src/main/java/castis/web/AgentController.java
@@ -1,13 +1,11 @@
 package castis.web;
 
-import castis.domain.point.PointHistoryDao;
 import castis.domain.model.AgentPointOnMonth;
 import castis.domain.model.PointHistory;
+import castis.domain.point.PointHistoryDao;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -24,55 +22,55 @@ import java.util.Map;
 @Controller
 @RequiredArgsConstructor
 public class AgentController {
-	
-	private PointHistoryDao pointHistoryDao;
-	private static final Logger logger = LoggerFactory.getLogger(AgentController.class);
-	
-	@RequestMapping(value = "agent_info/{currentYear}/{currentMonth}", method = RequestMethod.GET)
-	public String MonthlyStat(@PathVariable("currentYear") int currentYear, @PathVariable("currentMonth") int currentMonth, Model model) {
-		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		String name = user.getUsername(); // get logged in username
-		List<PointHistory> pointHistoryList = pointHistoryDao.getPointHistoryByUser(name);
-		int totalPoint = 0;
-		for (PointHistory pHistory : pointHistoryList) {
-			if (pHistory.getUsedate() != null ) {
-				//add only used code
-				totalPoint += pHistory.getPoint();
-			}
-		}
-		int userLevel = (int) Math.sqrt((int)(totalPoint/1000));
-		String totalPointStr = String.format("%,d", totalPoint);
-		model.addAttribute("userpoint", totalPointStr);
-		model.addAttribute("username", name);
-		model.addAttribute("userlevel", userLevel);
 
-		logger.info("this is 'agent_info" + "/" + currentYear + "/" + currentMonth + "'");
+    private PointHistoryDao pointHistoryDao;
+    private static final Logger logger = LoggerFactory.getLogger(AgentController.class);
 
-		// [key]username, [value]userStatOnMonth object list
-		Map<String, List<AgentPointOnMonth>> agentStatMap = new HashMap<String, List<AgentPointOnMonth>>();
-		List<PointHistory> pointHistoryByMonth = pointHistoryDao.getPointHistoryByYearAndMonth(currentYear, currentMonth);
-		for (PointHistory ph : pointHistoryByMonth) {
-			if (ph!= null && ph.getCode().equalsIgnoreCase("AUTO")) {
-				continue;
-			}
-			if (ph.getSender().equalsIgnoreCase("kskim") || ph.getSender().equalsIgnoreCase("awesomehan77") || ph.getSender().equalsIgnoreCase("donghyun")  || ph.getSender().equalsIgnoreCase("test")) {
-				// calculate only agent point
-				if (agentStatMap.containsKey(ph.getSender()) == true) {
-					// already in map for user
-					List<AgentPointOnMonth> madeList = agentStatMap.get(ph.getSender());
-					madeList.add(new AgentPointOnMonth(ph.getSender(), ph.getRecver(), ph.getMemo(), ph.getPoint(), ph.getUsedate().toString()));
-				} else {
-					// insert first time
-					AgentPointOnMonth agentObj = new AgentPointOnMonth(ph.getSender(), ph.getRecver(), ph.getMemo(), ph.getPoint(), ph.getUsedate().toString());
-					List<AgentPointOnMonth> newUserPointList = new ArrayList<AgentPointOnMonth>();
-					newUserPointList.add(agentObj);
-					agentStatMap.put(ph.getSender(), newUserPointList);
-				}
-			}
-		}
-		model.addAttribute("year", currentYear);
-		model.addAttribute("month", currentMonth);
-		model.addAttribute("agentStatMap", agentStatMap);
-		return "agent_info";
-	}
+    @RequestMapping(value = "agent_info/{currentYear}/{currentMonth}", method = RequestMethod.GET)
+    public String MonthlyStat(@PathVariable("currentYear") int currentYear, @PathVariable("currentMonth") int currentMonth, Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String name = user.getUsername(); // get logged in username
+        List<PointHistory> pointHistoryList = pointHistoryDao.getPointHistoryByUser(name);
+        int totalPoint = 0;
+        for (PointHistory pHistory : pointHistoryList) {
+            if (pHistory.getUsedate() != null) {
+                //add only used code
+                totalPoint += pHistory.getPoint();
+            }
+        }
+        int userLevel = (int) Math.sqrt((int) (totalPoint / 1000));
+        String totalPointStr = String.format("%,d", totalPoint);
+        model.addAttribute("userpoint", totalPointStr);
+        model.addAttribute("username", name);
+        model.addAttribute("userlevel", userLevel);
+
+        logger.info("this is 'agent_info" + "/" + currentYear + "/" + currentMonth + "'");
+
+        // [key]username, [value]userStatOnMonth object list
+        Map<String, List<AgentPointOnMonth>> agentStatMap = new HashMap<String, List<AgentPointOnMonth>>();
+        List<PointHistory> pointHistoryByMonth = pointHistoryDao.getPointHistoryByYearAndMonth(currentYear, currentMonth);
+        for (PointHistory ph : pointHistoryByMonth) {
+            if (ph != null && ph.getCode().equalsIgnoreCase("AUTO")) {
+                continue;
+            }
+            if (ph.getSender().equalsIgnoreCase("kskim") || ph.getSender().equalsIgnoreCase("awesomehan77") || ph.getSender().equalsIgnoreCase("donghyun") || ph.getSender().equalsIgnoreCase("test")) {
+                // calculate only agent point
+                if (agentStatMap.containsKey(ph.getSender()) == true) {
+                    // already in map for user
+                    List<AgentPointOnMonth> madeList = agentStatMap.get(ph.getSender());
+                    madeList.add(new AgentPointOnMonth(ph.getSender(), ph.getRecver(), ph.getMemo(), ph.getPoint(), ph.getUsedate().toString()));
+                } else {
+                    // insert first time
+                    AgentPointOnMonth agentObj = new AgentPointOnMonth(ph.getSender(), ph.getRecver(), ph.getMemo(), ph.getPoint(), ph.getUsedate().toString());
+                    List<AgentPointOnMonth> newUserPointList = new ArrayList<AgentPointOnMonth>();
+                    newUserPointList.add(agentObj);
+                    agentStatMap.put(ph.getSender(), newUserPointList);
+                }
+            }
+        }
+        model.addAttribute("year", currentYear);
+        model.addAttribute("month", currentMonth);
+        model.addAttribute("agentStatMap", agentStatMap);
+        return "agent_info";
+    }
 }
--- src/main/java/castis/web/ApiController.java
+++ src/main/java/castis/web/ApiController.java
@@ -2,8 +2,6 @@ package castis.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,12 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ApiController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
-	
-	@RequestMapping(value = "/api/v1/ping", method = RequestMethod.GET)
-	public String apiPing(HttpServletRequest request) {
-		logger.info("[API] apiPing");
-		return "pong.. make this when needs...Open api";
-	}
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
+
+    @RequestMapping(value = "/api/v1/ping", method = RequestMethod.GET)
+    public String apiPing(HttpServletRequest request) {
+        logger.info("[API] apiPing");
+        return "pong.. make this when needs...Open api";
+    }
 }
