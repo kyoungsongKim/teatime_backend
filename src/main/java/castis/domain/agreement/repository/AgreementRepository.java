1b362d34798d226c679a28d4fb2a5e6dbda4656a
HaTaejin <htj1222@gmail.com>
2024-07-08 11:55:32 +0900

    1.0.8_FIX

--- src/main/java/castis/domain/agreement/repository/AgreementRepository.java
+++ src/main/java/castis/domain/agreement/repository/AgreementRepository.java
@@ -18,13 +18,13 @@ public interface AgreementRepository
 
     @Query(
             nativeQuery = true,
-            value = "SELECT u.userid as userId, u.realname as realName, IFNULL(SUM(a.amount),0) as totalAmount, (SELECT a2.amount FROM agreement a2 WHERE a2.user_id = ?1 AND a2.type = 'GUARANTEE') as guaranteeAmount FROM agreement a RIGHT OUTER JOIN users u ON a.user_id = u.userid AND a.type != 'GUARANTEE' AND a.end_date >= ?2 WHERE u.userid = ?1 AND u.teamname LIKE 'Teatime' GROUP BY  u.userid, u.realname"
+            value = "SELECT u.userid as userId, u.realname as realName, IFNULL(SUM(a.amount),0) as totalAmount, (SELECT a2.amount FROM agreement a2 WHERE a2.user_id = ?1 AND a2.type = 'GUARANTEE') as guaranteeAmount FROM agreement a RIGHT OUTER JOIN users u ON a.user_id = u.userid AND a.type != 'GUARANTEE' AND a.end_date >= ?2 WHERE u.userid = ?1 AND u.teamname LIKE '사람' GROUP BY  u.userid, u.realname"
     )
     IUserAgreementInfo findUserAgreementInfo(String userId, LocalDate minDate);
 
     @Query(
             nativeQuery = true,
-            value = "SELECT userid as userId, u.realname as realName, IFNULL(SUM(a.amount),0) as totalAmount, (SELECT a2.amount FROM agreement a2 WHERE a2.user_id = userid AND a2.type = 'GUARANTEE' LIMIT 1) as guaranteeAmount FROM agreement a RIGHT OUTER JOIN users u ON a.user_id = userid AND a.type != 'GUARANTEE' AND a.end_date >= ?1 WHERE u.teamname LIKE 'Teatime' GROUP BY userid, u.realname"
+            value = "SELECT userid as userId, u.realname as realName, IFNULL(SUM(a.amount),0) as totalAmount, (SELECT a2.amount FROM agreement a2 WHERE a2.user_id = userid AND a2.type = 'GUARANTEE' LIMIT 1) as guaranteeAmount FROM agreement a RIGHT OUTER JOIN users u ON a.user_id = userid AND a.type != 'GUARANTEE' AND a.end_date >= ?1 WHERE u.teamname LIKE '사람' GROUP BY userid, u.realname"
     )
     List<IUserAgreementInfo> findUserAgreementInfoList(LocalDate minDate);
 }
