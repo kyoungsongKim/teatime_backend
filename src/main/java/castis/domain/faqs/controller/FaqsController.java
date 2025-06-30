3a5bcf7b8e0da15b3922a8ea429eb9975d651172
mcalba <kskim0428@castis.com>
2025-06-30 16:40:05 +0900

    1. 약관 추가 설정 추가 : terms:   file-path: /usr/local/imsfileupload/SaramTerms.pdf

--- src/main/java/castis/domain/faqs/controller/FaqsController.java
+++ src/main/java/castis/domain/faqs/controller/FaqsController.java
@@ -6,12 +6,21 @@ import castis.domain.faqs.service.FaqsService;
 import castis.domain.security.jwt.AuthProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -22,6 +31,10 @@ public class FaqsController {
 
     private final FaqsService faqService;
     private final AuthProvider authProvider;
+
+    @Value("${terms.file-path}")
+    private String termsFilePath;
+
 
     @GetMapping("")
     public List<Faqs> getFaqs() {
@@ -65,5 +78,16 @@ public class FaqsController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
+    }
+
+
+    @GetMapping("/terms")
+    public ResponseEntity<Resource> getTermsPdf(HttpServletResponse response) throws IOException {
+        Path path = Paths.get(termsFilePath);
+        Resource resource = new UrlResource(path.toUri());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(resource);
     }
 }
